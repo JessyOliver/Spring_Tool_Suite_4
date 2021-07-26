d422a3c4c7aec861156c510359370810e685447e
JessyOliver <jessy22oliver@gmail.com>
2021-07-26 16:28:29 -0300

    atividade dia 26 de julho de 2021, para o Blog Pessoal

---
+++ blogpessoalje/src/main/java/br/org/generation/blogpessoalje/controller/PostagemController.java
@@ -0,0 +1,31 @@
+package br.org.generation.blogpessoalje.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.generation.blogpessoalje.model.Postagem;
+import br.org.generation.blogpessoalje.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll (){
+		
+		return ResponseEntity.ok(postagemRepository.findAll()); 
+		
+	}
+	
+
+}
---
+++ blogpessoalje/src/test/java/br/org/generation/blogpessoalje/BlogpessoaljeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.blogpessoalje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpessoaljeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
